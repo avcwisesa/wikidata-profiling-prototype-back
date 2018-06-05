05205a168054e53917f8d08d04f2956530025319
Avicenna <avcwisesa@gmail.com>
2018-06-05 14:47:31 +0700

    adjust for local development

--- main.go
+++ main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using system ENV only.")
+	}
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
+	router := gin.Default()
 	router.Use(gin.Logger())
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "ProWD prototype database API", nil)
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, "ProWD prototype database API")
 	})
 
 	router.Run(":" + port)
