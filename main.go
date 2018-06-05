44a8833d805b7f3ac53b54d25ddd83a28aa4de0a
Avicenna <avcwisesa@gmail.com>
2018-06-05 14:36:53 +0700

    init for database API

--- main.go
+++ main.go
@@ -18,11 +18,9 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "ProWD prototype database API", nil)
 	})
 
 	router.Run(":" + port)
